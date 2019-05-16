8a20c894d143c32a25551a47229a984f8921d5e9
Mauricio Contreras Canepa <virtual.kur2@gmail.com>
2019-05-16 21:32:45 +0200

    review code

--- src/index.js
+++ src/index.js
@@ -6,21 +6,21 @@ const app = server();
 console.info('Iniciando aplicación...');
 console.info('*** Conectando a base de datos...')
 db.sync()
-  .then(() => {
-    console.info('*** ... conexión establecida.');
-    console.info('===> Iniciando servidor...');
-    app.listen(config.PORT, (err) => {
-      if(err) {
-        console.error('===> ... ha ocurrido un error al iniciar el servidor.')
-        console.error(err.message);
-        process.exit(1);
-      }
-      console.info('===> ... servidor iniciado y escuchando en el puerto: ', config.PORT);
-    });
-  })
-  .catch((err) => {
-    console.error('*** ... no se pudo establecer conexión.')
-    console.error(err.message);
-    console.info('Saliendo de aplicación...')
-    process.exit(1);
+.then(() => {
+  console.info('*** ... conexión establecida.');
+  console.info('===> Iniciando servidor...');
+  app.listen(config.PORT, (err) => {
+    if(err) {
+      console.error('===> ... ha ocurrido un error al iniciar el servidor.')
+      console.error(err.message);
+      process.exit(1);
+    }
+    console.info('===> ... servidor iniciado y escuchando en el puerto: ', config.PORT);
   });
+})
+.catch((err) => {
+  console.error('*** ... no se pudo establecer conexión.')
+  console.error(err.message);
+  console.info('Saliendo de aplicación...')
+  process.exit(1);
+});
